9778bfa3d178436023a8a7ade70a7fe7c447836b
Ryan <ryank9462@gmail.com>
2022-03-03 08:37:43 -0600

    Add number display functionality.

--- script.js
+++ script.js
@@ -27,8 +27,30 @@ const operate = (operator, num1, num2) => {
   return operator(num1, num2);
 }
 
-// Button click animation
+
 const buttons = document.querySelectorAll(".button");
+const numberButtons = document.querySelectorAll(".number");
+const display = document.querySelector(".display");
+const clearButton = document.querySelector("#clear");
+
+let displayContent = "";
+let enteredNumber = "";
+
+const updateDisplay = (text) => {
+  // Don't update the display directly, but update entered Number,
+  // then update the display with the enteredNumber.
+  enteredNumber += text;
+  displayContent = enteredNumber;
+  display.innerText = displayContent;
+}
+
+const clearDisplay = () => {
+  displayContent = "0";
+  entireNumber = "";
+  display.innerText = displayContent;
+}
+
+// Button click animation
 buttons.forEach((button) => {
   button.addEventListener('mousedown', (e) => {
     e.target.classList.add('clicked');
@@ -36,4 +58,21 @@ buttons.forEach((button) => {
   button.addEventListener('mouseup', (e) => {
     e.target.classList.remove('clicked');
   })
+})
+
+// Populate screen when number buttons are pressed
+numberButtons.forEach((numberButton) => {
+  numberButton.addEventListener('mousedown', (e) => {
+    // If there's a single 0 on display, and when 0 is clicked,
+    // Don't add another 0
+    if (e.target.innerText === "0" && displayContent === "0") {
+      return
+    }
+    updateDisplay(e.target.innerText);
+  })
+})
+
+// Clear display when C button is clicked
+clearButton.addEventListener('click', () => {
+  clearDisplay();
 })
