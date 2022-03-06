aee7ea44cfbb1b2853a8a37c65c019507331de54
Ryan Kim <ryank9462@gmail.com>
2022-03-05 19:49:17 -0600

    Complete functional simple calculator.

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const buttons = document.querySelectorAll(".button");
+const numberButtons = document.querySelectorAll(".number");
+const display = document.querySelector(".display");
+const clearButton = document.querySelector("#clear");
+const addButton = document.querySelector('#addition');
+const subtractButton = document.querySelector('#subtraction');
+const multiplyButton = document.querySelector('#multiplication');
+const divideButton = document.querySelector('#division');
+const equalButton = document.querySelector('#equals');
+
+let displayValue = "";
+let result;
+let currentOperation;
+let operatorButtonClicked = false;
+
+// Button click animation
+buttons.forEach((button) => {
+  button.addEventListener('mousedown', (e) => {
+    e.target.classList.add('clicked');
+  })
+  button.addEventListener('mouseup', (e) => {
+    e.target.classList.remove('clicked');
+  })
+})
+
 // Operator functions
 const add = (...nums) => {
   return nums.reduce((a, b) => {
@@ -27,110 +52,73 @@ const operate = (operator, num1, num2) => {
   return operator(num1, num2);
 }
 
-
-const buttons = document.querySelectorAll(".button");
-const numberButtons = document.querySelectorAll(".number");
-const display = document.querySelector(".display");
-const clearButton = document.querySelector("#clear");
-const addButton = document.querySelector('#addition');
-const subtractButton = document.querySelector('#subtraction');
-const multiplyButton = document.querySelector('#multiplication');
-const divideButton = document.querySelector('#division');
-const equalButton = document.querySelector('#equals');
-
-let displayContent = "";
-let enteredNumber = ""; // Variable where the display number will be stored
-let operandA;
-let operandB;
-let currentOperation;
-
-const updateDisplay = (text) => {
-  // Don't update the display directly, but update entered Number,
-  // then update the display with the enteredNumber.
-  enteredNumber += text;
-  displayContent = enteredNumber;
-  display.innerText = displayContent;
+const reset = () => {
+  displayValue = "";
+  result = null;
+  currentOperation = null;
+  operatorButtonClicked = false;
+  populateDisplay(displayValue);
 }
 
-const clearDisplay = () => {
-  displayContent = "0";
-  enteredNumber = "";
-  display.innerText = displayContent;
+const populateDisplay = (value) => {
+  display.innerText = value;
 }
 
-// Button click animation
-buttons.forEach((button) => {
-  button.addEventListener('mousedown', (e) => {
-    e.target.classList.add('clicked');
-  })
-  button.addEventListener('mouseup', (e) => {
-    e.target.classList.remove('clicked');
-  })
-})
-
-// Populate screen when number buttons are pressed
-numberButtons.forEach((numberButton) => {
-  numberButton.addEventListener('mousedown', (e) => {
-    // If there's a single 0 on display, and when 0 is clicked,
-    // Don't add another 0
-    if (e.target.innerText === "0" && displayContent === "0") {
-      return
+const evaluateSoFar = (operator) => {
+  if (!result) {
+    result = Number(displayValue);
+  } else {
+    if (currentOperation == divide && displayValue == 0) {
+      reset();
+      alert("Cannot divide by 0");
     }
-    updateDisplay(e.target.innerText);
+    result = operate(operator, result, Number(displayValue));
+  }
+  populateDisplay(result);
+}
+
+const evalAndSetCurrentOperation = (operator) => {
+  operatorButtonClicked = true;
+  evaluateSoFar(currentOperation);
+  currentOperation = operator;
+}
+
+// Number button click behavior
+numberButtons.forEach(numberButton => {
+  numberButton.addEventListener('click', (e) => {
+    if (operatorButtonClicked) {
+      displayValue = "";
+      operatorButtonClicked = false;
+    }
+    displayValue += e.target.innerText;
+    populateDisplay(displayValue);
   })
 })
 
-// Clear display when C button is clicked
-clearButton.addEventListener('click', () => {
-  clearDisplay();
+// Operator button click behavior
+addButton.addEventListener('click', (e) => {
+  console.log(e);
+  evalAndSetCurrentOperation(add);
 })
 
-// When an operator button is clicked, 
-// number currently on display will be stored in operandA variable
-// and the what kind of operation will be stored in whatOperation variable
-// enteredNumber will be reset to 0, but displayNumber will only be updated
-// when a new number is entered.
-// When the user enteres the second number and hits equals sign, 
-// The second number will be stored into operandB variable,
-// Operate function will be called with  
-
-// If another opertor is clicked instead of the equal button and there is already operand A, 
-// Result of the previous operation is stored in operandA
-
-addButton.addEventListener('click', () => {
-  operandA = Number(enteredNumber);
-  enteredNumber = "";
-  currentOperation = add;
+subtractButton.addEventListener('click', (e) => {
+  evalAndSetCurrentOperation(subtract);
 })
 
-subtractButton.addEventListener('click', () => {
-  operandA = Number(enteredNumber);
-  enteredNumber = "";
-  currentOperation = subtract;
+multiplyButton.addEventListener('click', (e) => {
+  evalAndSetCurrentOperation(multiply);
 })
 
-multiplyButton.addEventListener('click', () => {
-  operandA = Number(enteredNumber);
-  enteredNumber = "";
-  currentOperation = multiply;
+divideButton.addEventListener('click', (e) => {
+  evalAndSetCurrentOperation(divide);
 })
 
-divideButton.addEventListener('click', () => {
-  operandA = Number(enteredNumber);
-  enteredNumber = "";
-  currentOperation = divide;
+equalButton.addEventListener('click', (e) => {
+  evaluateSoFar(currentOperation);
+  console.log(result);
+  displayValue = null;
 })
 
-equalButton.addEventListener('click', () => {
-  operandB = Number(enteredNumber);
-  if (currentOperation === divide && operandB === 0) {
-    alert("Cannot divide by 0. Try again.");
-    currentOperation = null;
-    operandA = null;
-    operandB = null;
-    return;
-  }
-  displayContent = operate(currentOperation, operandA, operandB);
-  display.innerText = displayContent;
-  enteredNumber = "";
+clearButton.addEventListener('click', (e) => {
+  reset();
 })
